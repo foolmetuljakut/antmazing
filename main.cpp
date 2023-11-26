b0329d49c9fb1660230b2df82e06f227297ac7f1
foolmetuljakut@TPE15G2 <foolmetuljakut@gmail.com>
2023-11-26 12:58:09 +0100

    test setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
+void ut01() {
+
+}
+
+void mainfn(int argc, char **argv) {
     std::cout << "lol" << std::endl;
+}
+
+int main(int argc, char **argv) {
+#ifdef MAIN
+    mainfn(argc, argv);
+#endif // MAIN
+#ifdef ANTMAZING_UT1
+    ut01();
+#endif // ANTMAZING_UT01
     return 0;
 }
+
